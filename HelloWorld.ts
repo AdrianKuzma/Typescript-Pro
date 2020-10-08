aee8d4e687a06acef548a8cbb358237246828ed5
AdrianKuzma <beaversvk@gmail.com>
2020-10-08 18:45:54 +0200

    Ulohy 1-8

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,3 +1,10 @@
+console.log(totalD(100,0.2,0.4))
+console.log(Values(3,4,3))
+console.log(Triangle(2, 3, 4))
+console.log(Length([1, 2, 3, 4]))
+console.log(Slices(11, 5, 2))
+console.log(checkPalindrom("mom"))
+console.log(differenceMinMax([10, 4, 1, 4, -10, -50, 32, 21]))
 function totalD (TowerH:number,HeighS:number,lengthS:number){
     const a:number = (TowerH /HeighS) * (HeighS + lengthS)
     return a
@@ -14,5 +21,42 @@ function Values(FirstN:number,SecondN:number,ThirdN:number){
     else 
         return 0
 }
-console.log(totalD(0.2,0.4,100))
-console.log(Values(3,4,3))
+function Slices(ts:number,p:number,se:number){
+    const x = p * se
+    if (x <= ts)
+        return true;
+    else 
+        return false;
+}
+function Length(Array: number[]){
+    for (var i = 0; i < Array.length; ++i) {
+        Array[i] = Array[i] * Array.length
+    }
+    return Array  
+}
+function Triangle (a:number,b:number,c:number) {
+    if (a + b > c && b + c > a && a + c > b)
+        return true;
+    else
+        return false;
+}
+function checkPalindrom (str:string) {
+    const str1:string=str
+    str == str.split('').reverse().join('')
+    if(str1 == str)
+        return true
+    else 
+        return false
+}
+function differenceMinMax(Arr: number[]) {
+    let max: number = Arr[0]
+    let min: number= Arr[0]
+    for(let i = 1; i < Arr.length; i++)
+    {
+        if(Arr[i] > max)
+            max = Arr[i];
+        else if(Arr[i] < min)
+            min = Arr[i];
+    }
+    return max-min
+}
--- out/HelloWorld.js
+++ out/HelloWorld.js
@@ -1,10 +1,14 @@
-console.log(totalD(100,0.2,0.4))
-console.log(3,4,3))
-function totalD(TowerH, HeighS, LengthS) {
-    var a = (TowerH / HeighS) * (HeighS + LengthS);
+console.log(totalD(100, 0.2, 0.4));
+console.log(Values(3, 4, 3));
+console.log(Triangle(2, 3, 4));
+console.log(Length([1, 2, 3, 4]));
+console.log(Slices(11, 5, 2));
+console.log(checkPalindrom("mom"));
+console.log(differenceMinMax([10, 4, 1, 4, -10, -50, 32, 21]));
+function totalD(TowerH, HeighS, lengthS) {
+    var a = (TowerH / HeighS) * (HeighS + lengthS);
     return a;
 }
-//Ukradol som marekovi 
 function Values(FirstN, SecondN, ThirdN) {
     if (FirstN == SecondN || FirstN == ThirdN) {
         if (SecondN == ThirdN)
@@ -17,5 +21,42 @@ function Values(FirstN, SecondN, ThirdN) {
     else
         return 0;
 }
-
+function Slices(ts, p, se) {
+    var x = p * se;
+    if (x <= ts)
+        return true;
+    else
+        return false;
+}
+function Length(Array) {
+    for (var i = 0; i < Array.length; ++i) {
+        Array[i] = Array[i] * Array.length;
+    }
+    return Array;
+}
+function Triangle(a, b, c) {
+    if (a + b > c && b + c > a && a + c > b)
+        return true;
+    else
+        return false;
+}
+function checkPalindrom(str) {
+    var str1 = str;
+    str == str.split('').reverse().join('');
+    if (str1 == str)
+        return true;
+    else
+        return false;
+}
+function differenceMinMax(Arr) {
+    var max = Arr[0];
+    var min = Arr[0];
+    for (var i = 1; i < Arr.length; i++) {
+        if (Arr[i] > max)
+            max = Arr[i];
+        else if (Arr[i] < min)
+            min = Arr[i];
+    }
+    return max - min;
+}
 //# sourceMappingURL=HelloWorld.js.map
