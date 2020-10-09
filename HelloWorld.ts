d737129b2b87a0d92361aa1f1f05875b4f73dd62
AdrianKuzma <beaversvk@gmail.com>
2020-10-09 23:05:06 +0200

    stylistic changes

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,18 +1,20 @@
 console.log(totalD(100,0.2,0.4))
-console.log(Values(3,4,3))
-console.log(Triangle(2, 3, 4))
-console.log(Length([1, 2, 3, 4]))
-console.log(Slices(11, 5, 2))
+console.log(values(3,4,3))
+console.log(triangle(2, 3, 4))
+console.log(length1([1, 2, 3, 4]))
+console.log(slices(11, 5, 2))
 console.log(checkPalindrom("mom"))
 console.log(rps("paper","rock"))
 console.log(differenceMinMax([10, 4, 1, 4, -10, -50, 32, 21]))
 console.log(waron([2, 8, 7, 5]))
-console.log(Rook("F5","C8"))
-function totalD (TowerH:number,HeighS:number,lengthS:number){
-    const a:number = (TowerH /HeighS) * (HeighS + lengthS)
+console.log(rook("F5","C8"))
+
+function totalD (towerH:number,heighS:number,lengthS:number):number{
+    const a:number = (towerH /heighS) * (heighS + lengthS)
     return a
 }
-function Values(fn:number,sn:number,tn:number){
+
+function values(fn:number,sn:number,tn:number):number{
     if(fn == sn && fn == tn)
         return 2
     else if (sn == tn)
@@ -20,27 +22,31 @@ function Values(fn:number,sn:number,tn:number){
     else 
     if(sn == tn)
         return 2
-}      
-function Slices(ts:number,p:number,se:number){
+}   
+
+function slices(ts:number,p:number,se:number):boolean{
     const x = p * se
     if (x <= ts)
         return true;
     else
         return false;
 }
-function Length(Array: number[]){
-    for (var i = 0; i < Array.length; ++i) {
-        Array[i] = Array[i] * Array.length
+
+function length1(array: number[]):number[]{
+    for (var i = 0; i < array.length; ++i) {
+        array[i] = array[i] * array.length
     }
-    return Array  
+    return array  
 }
-function Triangle (a:number,b:number,c:number) {
+
+function triangle (a:number,b:number,c:number):boolean {
     if (a + b > c && b + c > a && a + c > b)
         return true;
     else
         return false;
 }
-function checkPalindrom (str:string) {
+
+function checkPalindrom (str:string):boolean {
     const str1:string=str
     str == str.split('').reverse().join('')
     if(str1 == str)
@@ -48,7 +54,8 @@ function checkPalindrom (str:string) {
     else
         return false
 }
-function rps(p1:string,p2:string){
+
+function rps(p1:string,p2:string):string{
     if (p1 == p2)
         return "TIE"
     else if (p1 == "rock" && p2 == "scissors")
@@ -59,19 +66,21 @@ function rps(p1:string,p2:string){
         return "p1 wins"
     else return "p2 wins"
 }
-function differenceMinMax(Arr: number[]) {
-    let max: number = Arr[0]
-    let min: number= Arr[0]
-    for(let i = 1; i < Arr.length; i++)
+
+function differenceMinMax(arr: number[]):number {
+    let max: number = arr[0]
+    let min: number= arr[0]
+    for(let i = 1; i < arr.length; i++)
     {
-        if(Arr[i] > max)
-            max = Arr[i];
-        else if(Arr[i] < min)
-            min = Arr[i];
+        if(arr[i] > max)
+            max = arr[i];
+        else if(arr[i] < min)
+            min = arr[i];
     }
     return max-min
 }
-function waron(arr: number[]){
+
+function waron(arr: number[]):number{
     let e:number = 0, o:number = 0
     for(var i = 0; i < arr.length;i++){
         if(i % 2 == 0)
@@ -85,7 +94,7 @@ function waron(arr: number[]){
         return e - o
 }
 
-function Rook(r1:String,r2:string){
+function rook(r1:String,r2:string):boolean{
     for(var i = 0; i < r1.length; i++){
         for(var j = 0; j < r2.length; j++){
             if(r1[i] == r2[i])
